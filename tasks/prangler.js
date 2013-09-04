ad8a59ad8166eab4e1072d2e071a8a41a7bc5346
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2013-09-04 18:12:12 +0300

    Made the task a multitask and use more grunt built-in helpers.

--- tasks/prangler.js
+++ tasks/prangler.js
@@ -8,42 +8,35 @@
 'use strict';
 
 module.exports = function (grunt) {
-  grunt.registerTask('prangler', 'Collect html templates and views to AngularJS $templateCache.', function () {
-    var templatePaths = [], targets = [], globs = [];
-    var configTemplate = "angular.module('<%= ngApp %>').run(['$templateCache', function($templateCache) {<%= loadScripts %>}]);";
+  grunt.registerMultiTask('prangler', 'Collect html templates and views to AngularJS $templateCache.', function () {
+    var configTemplate = "angular.module('<%= ngApp %>').run(['$templateCache', function($templateCache) {<%= generatedScript %>}]);";
     var putTemplate = "$templateCache.put('<%= key %>', '<%= template %>');\n";
     var str = require('string');
 
-    grunt.config.requires('prangler');
-    grunt.config.requires('prangler.options.ngApp');
-    grunt.config.requires('prangler.files');
-    
-    grunt.log.subhead('Collect templates for AngularJS module \'' + grunt.config('prangler.options.ngApp') + '\'\n');
-    
-    var filesConfig = grunt.config('prangler.files');
+    var options = this.options({
+      ngApp: 'app',
+      stripPathForTemplateId: ''
+    });
 
-    for (var key in filesConfig) {
-      targets.push(key);
-      globs.push(filesConfig[key]);
-    }
+    grunt.log.subhead('Collect templates for AngularJS module \'' + options.ngApp + '\'\n');
 
-    templatePaths = grunt.file.expand(globs[0]);
-  
-    var loadScripts = '';
-    for (var path in templatePaths) {
-      var temp = {
-        key: templatePaths[path].replace(grunt.config('prangler.options.stripPathForTemplateId'), '').replace('\\', '/'),
-        template: str(grunt.file.read(templatePaths[path])).collapseWhitespace().replace(/([^'\\]*(?:\\[\s\S][^'\\]*)*)'/g, "$1\\'")
-      };
-      grunt.log.writeln(temp.key);
-      loadScripts = loadScripts + grunt.template.process(putTemplate, {data: temp});
-    }
+    this.files.forEach(function (template) {
+      var generatedScript = template.src.map(function (path) {
+        var temp = {
+          key: path.replace(options.stripPathForTemplateId, '').replace('\\', '/'),
+          template: str(grunt.file.read(path)).collapseWhitespace().replace(/([^'\\]*(?:\\[\s\S][^'\\]*)*)'/g, "$1\\'")
+        };
+        grunt.log.writeln(temp.key);
+        return grunt.template.process(putTemplate, {data: temp});
+      }).join('');
 
-    grunt.file.write(targets[0], grunt.template.process(configTemplate, {data: {
-      ngApp: grunt.config('prangler.options.ngApp'),
-      loadScripts: loadScripts
-    }}));
+      grunt.file.write(template.dest, grunt.template.process(configTemplate, {data: {
+        ngApp: options.ngApp,
+        generatedScript: generatedScript
+      }}));
 
-    grunt.log.ok(targets[0] + ' has ' + templatePaths.length + ' templates.');
+      grunt.log.ok(template.dest + ' has ' + template.src.length + ' templates.');
+    });
+
   });
 };
